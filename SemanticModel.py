c66cdd1f8c27ec7801e238499e6fbf049f1fae38
Anirban Das <anirban.nick@gmail.com>
2013-04-04 02:04:32 +0530

    Added SemanticModel.py

---
+++ SemanticModel.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+SemanticModel.py
+
+Created by nick on 2013-04-04.
+Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+
+
+
