b0a52b7b80e44c26eb5b3d65b96f161a04552b7f
Anirban Das <anirban.nick@gmail.com>
2013-04-04 16:01:38 +0530

    added code in prolog.py

--- prolog.py
+++ prolog.py
@@ -7,14 +7,27 @@ Created by nick on 2013-04-04.
 Copyright (c) 2013 __MyCompanyName__. All rights reserved.
 """
 
-import sys
-import os
+def  covert(z):
+	#
+	#	It takes as input a logical form, z ( meanignful representation)
+	#	in its functional form and converts
+	#	it into a first order logical form
+	#	of geoquery query language
+	#
+	#	It returns the converted first-order-logical-form,z
+	#
+	return z
+	
+	
 
-
-def main():
-	pass
-
-
-if __name__ == '__main__':
-	main()
+def  execute(z):
+	#
+	#	It takes as input a first-order-logical-form
+	#	and executes it in the geoquery database.
+	#
+	#	It returns the result after executing the query
+	#	in the geoquery database.
+	#
+	return result
+	
 
