91836e9804288b4be46bdde316d114b95ccb015c
Anirban Das <anirban.nick@gmail.com>
2013-04-04 15:27:52 +0530

    Added prolog.py

---
+++ prolog.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+prolog.py
+
+Created by nick on 2013-04-04.
+Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+"""
+
+import sys
+import os
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
+
